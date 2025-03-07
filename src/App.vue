c7f73e3db4b020d2a9566f44882ba18b3ad681f2
hel1_yeah <hel1_yeah@ukr.net>
2025-03-07 15:26:42 +0200

    add all table

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,21 @@
 	<div class="p-4">
 		<h1>Parent</h1>
 		<HelloWorld
-			:tableData="mockDataLikeProps"
+			:tableData="ReceivingFiatFromCustomer"
 			:tableName="'ReceivingFiatFromCustomer'"
 		/>
 		<HelloWorld
-			:tableData="mockDataLikeProps2"
+			:tableData="ReceivingCryptoFromCustomer"
 			:tableName="'ReceivingCryptoFromCustomer'"
+		/>
+		<HelloWorld :tableData="ReceiptOfOwnFiat" :tableName="'ReceiptOfOwnFiat'" />
+		<HelloWorld
+			:tableData="CurrencyConversionOfYourOwnFiat"
+			:tableName="'CurrencyConversionOfYourOwnFiat'"
+		/>
+		<HelloWorld
+			:tableData="BringingOutYourOwnForFiat"
+			:tableName="'BringingOutYourOwnForFiat'"
 		/>
 	</div>
 </template>
@@ -101,7 +110,7 @@ const calculateDifference = (num1, num2) => {
 // 	],
 // ]);
 
-const mockDataLikeProps = reactive([
+const ReceivingFiatFromCustomer = reactive([
 	{
 		id: 1,
 		header: 'Fiat Acceptance from Client',
@@ -261,36 +270,8 @@ const mockDataLikeProps = reactive([
 		dependsOn: [11, 13],
 		target: 18,
 	},
-	// {
-	// 	id: 4,
-	// 	header: 'num 1',
-	// 	value: 10,
-	// 	noEditable: false,
-	// 	input_type: 'number',
-	// 	action: sumFunction, // Функція для @input
-	// 	dependsOn: [3, 4],
-	// 	target: 5,
-	// },
-	// {
-	// 	id: 5,
-	// 	header: 'num 2',
-	// 	value: 20,
-	// 	noEditable: false,
-	// 	input_type: 'number',
-	// 	action: sumFunction, // Функція для @input
-	// 	dependsOn: [3, 4],
-	// 	target: 5,
-	// },
-	// {
-	// 	id: 5,
-	// 	header: 'sum 1 & 2',
-	// 	value: 0,
-	// 	noEditable: true,
-	// 	input_type: 'number',
-	// },
 ]);
-
-const mockDataLikeProps2 = reactive([
+const ReceivingCryptoFromCustomer = reactive([
 	{
 		id: 1,
 		header: 'Acceptance of Crypto from a customer',
@@ -460,32 +441,242 @@ const mockDataLikeProps2 = reactive([
 		dependsOn: [11, 13],
 		target: 19,
 	},
-	// {
-	// 	id: 4,
-	// 	header: 'num 1',
-	// 	value: 10,
-	// 	noEditable: false,
-	// 	input_type: 'number',
-	// 	action: sumFunction, // Функція для @input
-	// 	dependsOn: [3, 4],
-	// 	target: 5,
-	// },
-	// {
-	// 	id: 5,
-	// 	header: 'num 2',
-	// 	value: 20,
-	// 	noEditable: false,
-	// 	input_type: 'number',
-	// 	action: sumFunction, // Функція для @input
-	// 	dependsOn: [3, 4],
-	// 	target: 5,
-	// },
-	// {
-	// 	id: 5,
-	// 	header: 'sum 1 & 2',
-	// 	value: 0,
-	// 	noEditable: true,
-	// 	input_type: 'number',
-	// },
+]);
+const ReceiptOfOwnFiat = reactive([
+	{
+		id: 1,
+		header: 'Receipt of own Fiat',
+		value: 'Bank transfer',
+		noEditable: false,
+		input_type: 'select',
+		select_options: ['Bank transfer'],
+		action: null,
+	},
+	{
+		id: 2,
+		header: 'Date',
+		value: '10.02.2025',
+		noEditable: false,
+		input_type: 'text',
+		action: setDate,
+	},
+	{
+		id: 3,
+		header: 'Bank name/type of operation/cash',
+		value: '№12345',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 4,
+		header: 'Type of currency deposited',
+		value: 'PLN',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 5,
+		header: 'Quantity of currency deposited	',
+		value: 'USDT',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 6,
+		header: 'Customer reference number',
+		value: '12345',
+		noEditable: false,
+		input_type: 'text',
+		focus: true,
+	},
+	{
+		id: 7,
+		header: 'Origin/name of exchange office	',
+		value: 'Origin/name',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 8,
+		header: "Depositor's data",
+		value: 'Data',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 9,
+		header: 'Deposit destination',
+		value: 'Santander',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+]);
+const CurrencyConversionOfYourOwnFiat = reactive([
+	{
+		id: 1,
+		header: 'Currency conversion of your own Fiat',
+		value: 'Bank transfer',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 2,
+		header: 'Date',
+		value: '10.02.2025',
+		noEditable: false,
+		input_type: 'text',
+		action: setDate,
+	},
+	{
+		id: 3,
+		header: 'Operation No.',
+		value: '№12345',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 4,
+		header: 'Initial currency type',
+		value: 'PLN',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 5,
+		header: 'Amount of initial currency',
+		value: 0,
+		noEditable: false,
+		input_type: 'number',
+		action: calculateRateFromPlnToEur,
+		dependsOn: [5, 7],
+		target: 8,
+		focus: true,
+	},
+	{
+		id: 6,
+		header: 'Target currency type',
+		value: 'EUR',
+		noEditable: false,
+		input_type: 'text',
+		focus: true,
+	},
+	{
+		id: 7,
+		header: 'Target currency exchange rate',
+		value: 4.14,
+		noEditable: false,
+		input_type: 'number',
+		action: calculateRateFromPlnToEur,
+		dependsOn: [5, 7],
+		target: 8,
+		focus: true,
+	},
+	{
+		id: 8,
+		header: 'Amount of target currency',
+		value: 0,
+		noEditable: false,
+		input_type: 'number',
+		action: null,
+	},
+	{
+		id: 9,
+		header: 'Supply location',
+		value: 'Wanda',
+		noEditable: true,
+		input_type: 'text',
+		action: null,
+	},
+]);
+const BringingOutYourOwnForFiat = reactive([
+	{
+		id: 1,
+		header: 'Bringing out your own for Fiat',
+		value: 'Bank transfer',
+		noEditable: false,
+		input_type: 'select',
+		select_options: ['Bank transfer'],
+		action: null,
+	},
+	{
+		id: 2,
+		header: 'Date',
+		value: '10.02.2025',
+		noEditable: false,
+		input_type: 'text',
+		action: setDate,
+	},
+	{
+		id: 3,
+		header: 'Transaction record number',
+		value: '№12345',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 4,
+		header: 'Name of bank/type of operation',
+		value: 'Przelew',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 5,
+		header: 'Type of currency paid out',
+		value: 0,
+		noEditable: false,
+		input_type: 'text',
+	},
+	{
+		id: 6,
+		header: 'Amount of currency paid out',
+		value: 'EUR',
+		noEditable: false,
+		input_type: 'text',
+		focus: true,
+	},
+	{
+		id: 7,
+		header: 'Rate of currency paid out',
+		value: 4.14,
+		noEditable: false,
+		input_type: 'number',
+		focus: true,
+	},
+	{
+		id: 8,
+		header: 'Target payout location',
+		value: 'Gotówka',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 9,
+		header: 'Origin/name of the exchange office',
+		value: 'Wanda',
+		noEditable: true,
+		input_type: 'text',
+		action: null,
+	},
+	{
+		id: 9,
+		header: 'Data of the paying agent',
+		value: 'M Głuchowska',
+		noEditable: false,
+		input_type: 'text',
+		action: null,
+	},
 ]);
 </script>
